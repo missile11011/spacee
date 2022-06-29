a983095ce088728af6a9431cd0c5d7e22a3143ed
Misael Reyes <misaelrey90@gmail.com>
2022-06-29 16:46:52 -0500

    api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Image } from "./image"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spacee_exercise';
+  readonly URL = "https://api.nasa.gov/planetary/apod?api_key=xXO1hYPWfoWcc7X7XjVZGwFTtEfHv2E2gNLKTwI5"
+
+  image!: any;
+
+  constructor(private http: HttpClient) {} 
+
+  getImage() {
+    this.http.get<any>(this.URL).subscribe(data => {
+        this.image = data
+    })
+    console.log(this.image)
+  }
 }
