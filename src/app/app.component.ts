d0875d833b5b643b6b42cc448055c52677e1b34f
Misael Reyes <misaelrey90@gmail.com>
2022-06-29 19:42:11 -0500

    landpage image and info of APOD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { Image } from "./image"
@@ -7,18 +7,22 @@ import { Image } from "./image"
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'spacee_exercise';
+  ngOnInit(){
+    this.getImage()
+    console.log("hello world") 
+  }
   readonly URL = "https://api.nasa.gov/planetary/apod?api_key=xXO1hYPWfoWcc7X7XjVZGwFTtEfHv2E2gNLKTwI5"
 
   image!: any;
-
+  
   constructor(private http: HttpClient) {} 
-
   getImage() {
     this.http.get<any>(this.URL).subscribe(data => {
         this.image = data
     })
     console.log(this.image)
   }
+
 }
---
+++ src/app/image.ts
@@ -0,0 +1,7 @@
+export interface Image {
+    copyright: string,
+    date: string,
+    explanation: string,
+    url: string,
+    title: string
+}
